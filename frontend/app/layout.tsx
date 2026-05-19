7de295b19112abba299e9b2153f5cdd3a910d4d9
Hardik Vatukiya <hardikvatukiya0014@gmail.com>
2026-05-19 14:28:06 +0530

    add google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -63,6 +63,7 @@ import { WebVitals } from "@/components/WebVitals";
 import HeroHeader from "@/components/Header";
 import { SanityLive } from "@/lib/sanity.live";
 import { getNavigation } from "@/lib/sanity.loader";
+import { GoogleAnalytics } from "@/components/google-analytics";
 
 export default async function RootLayout({
   children,
@@ -117,6 +118,7 @@ export default async function RootLayout({
       <body className="min-h-full flex flex-col relative">
         <div className="fixed inset-0 -z-10 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
         <div className="fixed inset-0 -z-10 bg-[radial-gradient(circle_800px_at_100%_200px,#d5e5ff10,transparent)]"></div>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
 
         <WebVitals />
         <SmoothScroll>
